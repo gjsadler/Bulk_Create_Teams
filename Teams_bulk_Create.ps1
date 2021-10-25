b29ef9e0c3f0ed74afb853053140a9c2a28eb755
gjsadler <50763466+gjsadler@users.noreply.github.com>
2021-10-25 13:08:51 -0500

    Add files via upload

---
+++ Teams_bulk_Create.ps1
@@ -0,0 +1,12 @@
+﻿Connect-MicrosoftTeams
+
+$Teams = Import-csv D:\Teams.csv
+
+Foreach ($team in $teams)
+
+	$displayName = $DisplayName.Teams
+	$description = $Description.Teams
+	$mailNickName = $MailNickName.Teams
+	$visibility = $Visibility.Teams
+
+New-Team -DisplayName $displayName -Description $description -MailNickName $mailNickName -Visibility $visibility
